a4a04aae5c3740879047b1afdef7206ed530c588
wooobo <irooori89@gmail.com>
2024-11-13 21:46:09 +0900

    feat: 계산을 담당하는 Enum Operation.kt 추가

---
+++ src/test/kotlin/calculate/Operation.kt
@@ -0,0 +1,47 @@
+package calculate
+
+enum class Operation(val symbol: String) {
+    PLUS("+") {
+        override fun apply(
+            first: Int,
+            second: Int,
+        ): Int {
+            return first + second
+        }
+    },
+    MINUS("-") {
+        override fun apply(
+            first: Int,
+            second: Int,
+        ): Int {
+            return first - second
+        }
+    },
+    MULTIPLY("*") {
+        override fun apply(
+            first: Int,
+            second: Int,
+        ): Int {
+            return first * second
+        }
+    },
+    DIVIDE("/") {
+        override fun apply(
+            first: Int,
+            second: Int,
+        ): Int {
+            return first / second
+        }
+    },
+    NONE("") {
+        override fun apply(
+            first: Int,
+            second: Int,
+        ): Int = throw IllegalArgumentException("Unknown operation")
+    }, ;
+
+    abstract fun apply(
+        first: Int,
+        second: Int,
+    ): Int
+}
