93215b5a69db30051f259b9f2d810164d5a6b5ec
wooobo <irooori89@gmail.com>
2024-11-15 14:28:06 +0900

    feat: main 함수 추가

---
+++ src/main/kotlin/race/Application.kt
@@ -0,0 +1,10 @@
+package race
+
+fun main() {
+    val carCountView = InputView("자동차 대수는 몇 대 인가요?")
+    val retryCountView = InputView("시도할 회수는 몇 회 인가요?")
+    val resultView = RaceResultView()
+
+    val raceBoard = RaceBoard(carCountView, retryCountView, resultView)
+    raceBoard.start()
+}
